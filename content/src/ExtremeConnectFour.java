cf00edb887357cdbc5d1c1b789de366039c10f9a
Colby Chance <duncanchance11@gmail.com>
2023-04-07 21:58:37 -0400

    Solve the problem

--- content/src/ExtremeConnectFour.java
+++ content/src/ExtremeConnectFour.java
@@ -7,44 +7,61 @@ class ExtremeConnectFour {
     }
 
     private static final int WIN_COUNT = 4;
-    private static final List<Color> RED_WIN = List.of(Color.RED, Color.RED, Color.RED, Color.RED);
-    private static final List<Color> BLACK_WIN = List.of(Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK);
+    private static final Map<Color, List<Color>> WIN_CONDITIONS = Map.of(
+        Color.RED, List.of(Color.RED, Color.RED, Color.RED, Color.RED),
+        Color.BLACK, List.of(Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK) 
+    );
 
     private final Map<Integer, List<Color>> board = new HashMap<>();
 
 
-    Optional<Color> move(int columnIndex, Color chipColor) {
-        // move
+    public Optional<Color> move(int columnIndex, Color chipColor) {
+        // Place chip
         board.computeIfAbsent(columnIndex, k -> new ArrayList<>()).add(0, chipColor);
 
         // Check vertical
-        if (board.get(columnIndex).equals(RED_WIN)) {
-            return Optional.of(Color.RED);
-        }
-
-        if (board.get(columnIndex).equals(BLACK_WIN)) {
-            return Optional.of(Color.BLACK);
+        var column = board.getOrDefault(columnIndex, new ArrayList<>());
+        var truncatedColumn = column.subList(0, Math.min(WIN_COUNT, column.size()));
+        if (WIN_CONDITIONS.get(chipColor).equals(truncatedColumn)) {
+            return Optional.of(chipColor);
         }
 
         // Check horizontal
+        for (int r = 0; r < column.size(); r++) {
+            var leftBound = columnIndex - WIN_COUNT + 1;
+            var rightBound = columnIndex + WIN_COUNT - 1;
+            var row = range(r, leftBound, rightBound);
+            for (var entry: WIN_CONDITIONS.entrySet()) {
+                if (Collections.indexOfSubList(row, entry.getValue()) != -1) {
+                    return Optional.of(entry.getKey());
+                }
+            }
+        }
+        
         return Optional.empty();
     }
 
-    Chip get(int column, int row) {
+    private List<Color> range(int rowIndex, int leftBound, int rightBound) {
+        var row = new ArrayList<Color>();
+        for (int c = leftBound; c <= rightBound; c++) {
+            row.add(get(c, rowIndex));
+        }
+        return row;
+    }
 
+    private Color get(int columnIndex, int rowIndex) {
+        var column = board.getOrDefault(columnIndex, new ArrayList<>());
+        if (column.size() <= rowIndex) {
+            return null;
+        }
+        return column.get(rowIndex);
     }
     
     public static void main(String[] args) {
         var board = new ExtremeConnectFour();
-        board.move(0, Color.RED);
-        board.move(0, Color.RED);
-        board.move(0, Color.RED);
-        System.out.println(board.move(0, Color.RED));
+        System.out.println(board.move(1, Color.RED));
+        System.out.println(board.move(2, Color.RED));
+        System.out.println(board.move(3, Color.RED));
+        System.out.println(board.move(4, Color.RED));
     }
 }
-
-/*
-Ideas:
-
-pattern match
-*/
